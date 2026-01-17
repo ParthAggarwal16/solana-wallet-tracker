bd6f6654dab617b72c8c081749f59ba4a9be0822
ParthAggarwal16 <parth22012006@gmail.com>
2026-01-17 20:16:51 +0530

    my first shot at path param and request query

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,23 +1,23 @@
-//fastify instance and plugins
+//fastify instance and plugins 
 
 import fastify from "fastify"
 
-const server = fastify()
+const server = fastify()        //here server is not necceserily a fastify server , its more of a instance or big object
+                                // with methods like .get , .post , .listen , .register
 
-//path parametre 
+//my first shot at path and query parametres
 
-server.get("/wallet/:id", async (request) => {
-    const id = request.params as { id : string }
-    return  {
-        walletID : id
-    }
-})
+server.get("/wallet/:id", async (request) => {                  //method used to register a route with path (in this case is "/wallet/:id")
+                                                                // with a handler (my function) , async means the function will return a prmoise 
+                                                                //then fastify will await it and send the resoved value as a HTTP response 
+                                                                //without async , i will have to manually send replies
+    const { id } = request.params as { id: string }             
+    const { network } = request.query as { network?: string }
 
-server.listen ({ port : 8080 })
-
-server.get("/wallet/:id?network=solana", async (request) => {
-    const networkName = request.query as { networkName : string}
     return {
-        foundNetwork : networkName
+        walletId: id,
+        network: network ?? "default"
     }
 })
+
+server.listen({ port: 8080 })
