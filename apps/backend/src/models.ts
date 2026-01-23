ed7dfa09815f20a1fba6ec24df1424c2e51c67bd
ParthAggarwal16 <parth22012006@gmail.com>
2026-01-23 18:10:28 +0530

    definig variants

---
+++ apps/backend/src/models.ts
@@ -0,0 +1,5 @@
+// required entities :
+// user : id, email, created at timestamp
+// wallet : id, userID, address, chain(solana), createdAt
+// tranaction state : signature, slot, walletAddress, blockTime, direction(incoming, outgoing), amount, fee, success (boolean)
+// ingestion : walletaddress, lastproccessedSlot, lastprocessedSignature, updatedAt
