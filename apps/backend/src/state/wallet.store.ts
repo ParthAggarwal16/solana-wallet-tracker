7f9f9bbed753784e018367b68430de57d782ca7c
ParthAggarwal16 <parth22012006@gmail.com>
2026-01-27 20:38:42 +0530

    initializing ingestatus and wallets storage

---
+++ apps/backend/src/state/wallet.store.ts
@@ -0,0 +1,10 @@
+// a place that stores wallets
+// a place that stores ingestionState
+
+// addWallet(userId, address)
+// removeWallet(walletId)
+// listWallets(userId)
+// countWallets(userId)
+
+
+
